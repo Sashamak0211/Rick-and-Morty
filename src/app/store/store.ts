215d0972398da96048fa1a14ac820f42f707b4ad
Sasha Vak <mak12121212@gmail.com>
2025-10-02 19:39:49 +0300

    Теперь точно добавлен стор

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { charactersSlice } from "./characterSlice";
+
 export const store = configureStore({
   reducer: {
-    
-  }
-})
+    characters: charactersSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
