6c3944441843bc373a210b1f2e062a4cfb555d19
Sashamak0211 <mak12121212@gmail.com>
2025-12-02 22:51:58 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { useTranslation } from 'react-i18next';
-
 import { CharacterList, CharacterPage, NotFoundPage } from '@/pages';
 import { Layout } from '@/shared';
 
 function App() {
-  const { t, i18n } = useTranslation();
-
-  console.log('CURRENT LANG =', i18n.language);
-  console.log('TEST TRANSLATION =', t('gender_male'));
   return (
     <>
       <Router basename="/Rick-and-Morty/">
